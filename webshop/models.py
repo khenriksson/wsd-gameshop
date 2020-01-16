2b99f2f963f5a008811fff310dde444f1a159a25
silviageier <silvia.geier@aalto.fi>
2020-01-16 15:42:04 +0200

    Game class added to admin interface

--- webshop/models.py
+++ webshop/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Game(models.Model):
+
     game_id = models.CharField(max_length=16)
     purchases = models.IntegerField()
     # developer = models.ForeignKey(User, on_delete=models.CASCADE)
     game_url = models.CharField(max_length=250)
     price = models.FloatField()
-
+    def __str__(self):
+        return self.game_id
