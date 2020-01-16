b05db3e4c09d302107f4cf9c4fee84edaa691159
Kasper Henriksson <kasperhenriksson@Kaspers-MacBook-Pro.local>
2020-01-16 21:17:54 +0200

    Template's working, CSS added and working, switching site URL NOT working.

--- webshop/views.py
+++ webshop/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Game
+
+# def webshop(request):
+#      all_games = Game.objects.all()
+#      html = ''
+#      for game in all_games:
+#         url = '/webshop/' + str(game.id) + '/'
+#         html += '<a href="' + url + '">' + str(game.game_id) + '</a><br>'
+#      return HttpResponse(html)
 
 def webshop(request):
-    return HttpResponse("Hello, World")
+    all_games = Game.objects.all()
+    template = loader.get_template('webshop/index.html')
+    context = {
+        'all_games': all_games,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, game_id):
-    return HttpResponse("<h2>Details for game_id" + str(game_id) + "</h2>")
+    return HttpResponse("<h2>Details for game_id: " + str(game_id) + "</h2>")
