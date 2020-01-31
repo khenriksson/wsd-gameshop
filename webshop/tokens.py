3bac27a05f60bf695354887fc2b446830f44f361
Kasper Henriksson <kasperhenriksson@Kaspers-MacBook-Pro.local>
2020-01-31 12:49:18 +0200

    Editing import six

--- webshop/tokens.py
+++ webshop/tokens.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six
 from django.utils.crypto import constant_time_compare, salted_hmac
+
+import six
 
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
