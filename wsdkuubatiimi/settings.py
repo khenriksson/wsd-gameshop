a874973d928da19fa5b638054589ba2c1fd6f7e5
Rasmus Laug <laugr1@aalto.fi>
2020-02-06 17:15:50 +0200

    Allauth- first stage

--- wsdkuubatiimi/settings.py
+++ wsdkuubatiimi/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'bootstrap4',
     'webshop',
     'rest_framework',
+    'django.contrib.sites',
+    
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +130,18 @@ STATIC_URL = '/static/'
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/webshop/'
 
-AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend')
+
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',
+'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = True
+
+
+
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mailtrap.io'
